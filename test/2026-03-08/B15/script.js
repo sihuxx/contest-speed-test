5170881094e5150d9ceb8634239fc763734710bd
sihuxx <sihu714@gmail.com>
2026-03-08 15:20:25 +0900

    20260308 뻉이또침

---
+++ test/2026-03-08/B15/script.js
@@ -0,0 +1,45 @@
+let state = { limit:10, page:1 }
+
+const data = await fetch("./sample-data.csv").then(res => res.text())
+const rows = data.split("\n").slice(1).map(line => `<tr>${line.split(",").map(ceil => `<td>${ceil}</td>`).join("")}</tr>`)
+
+const tableBody = $("#tableBody")
+const pageBtns = $$(".pagination-btn")
+const prevBtn = $(".prev-btn")
+const nextBtn = $(".next-btn")
+
+function setState(newState) {
+  state =  {...state, ...newState}
+  render()
+}
+
+pageBtns.forEach(btn => btn.addEventListener("click", () => {
+  setState({ page: Number(btn.textContent) })
+}))
+prevBtn.addEventListener("click", () => {
+  setState({ page: state.page - 1 })
+})
+nextBtn.addEventListener("click", () => {
+  setState({ page: state.page + 1 })
+})
+
+function render() {
+  const range = state.limit * (state.page - 1)
+  tableBody.innerHTML = rows.slice(range, range + state.limit).join("")
+  
+  prevBtn.disabled = state.page === 1
+  nextBtn.disabled = state.page === 5
+
+  pageBtns.forEach((btn, i) => {
+    btn.classList.remove("active")
+    btn.classList.remove("page-info")
+    btn.textContent = i + 1
+
+    if(i + 1 === state.page) btn.classList.add("active")
+    if(i === 1 && state.page === 5 || i === 3 && state.page === 1) {
+      btn.classList.add("page-info")
+      btn.textContent = "..."
+    }
+  })
+}
+render()
