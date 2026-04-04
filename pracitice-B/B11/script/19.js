634c916546bb1167af6da3aeb64b498d83932113
sihuxx <sihu714@gmail.com>
2026-04-05 00:48:06 +0900

    2026-04-05

---
+++ pracitice-B/B11/script/19.js
@@ -0,0 +1,23 @@
+let startTime = 0, elapsed = 0, id = null, run = false
+
+function timer(now) {
+    const time = now - startTime + elapsed
+
+    minutes.innerHTML = String(Math.floor(time / 60000)).padStart(2, 0)
+    second.innerHTML = String(Math.floor((time % 60000) / 1000)).padStart(2, 0)
+    mili.innerHTML = String(Math.floor(time % 1000)).padStart(3, 0)
+
+    id = requestAnimationFrame(timer)
+}
+function render() {
+    run = !run
+    if(run) {
+        startTime = performance.now()
+        id = requestAnimationFrame(timer)
+        container.innerHTML = `<button onclick="render()">중지</button>`
+    } else {
+        elapsed += performance.now() - startTime
+        cancelAnimationFrame(id)
+        container.innerHTML = `<button onclick="render()">계속</button>`
+    }
+}
---
+++ pracitice-B/B15/script/11.js
@@ -0,0 +1,40 @@
+let state = {page: 1, limit: 10}
+
+const data = await fetch("./sample-data.csv").then(res => res.text())
+const rows = data.split("\n").slice(1).map(line => `<tr>${line.split(",").map(ceil => `<td>${ceil}</td>`).join("")}</tr>`)
+
+const tableBody = $("#tableBody")
+const prevBtn = $(".prev-btn")
+const nextBtn = $(".next-btn")
+const pageBtns = $$(".pagination-btn")
+
+function setState(newState) {
+    state = {...state, ...newState}
+    render()
+}
+pageBtns.forEach(btn => btn.addEventListener("click", () => {
+    setState({ page: Number(btn.textContent) })
+}))
+prevBtn.addEventListener("click", () => {
+    setState({ page: state - 1 })
+})
+nextBtn.addEventListener("click", () => {
+    setState({ page: state + 1 })
+})
+
+function render() {
+    const range = state.limit * (state.page - 1)
+    tableBody.innerHTML = rows.slice(range, range + state.limit).join("")
+    prevBtn.disabled = state.page === 1
+    nextBtn.disabled = state.page === 5
+    pageBtns.forEach((btn, i) => {
+        btn.classList.remove("active")
+        btn.classList.remove("page-info")
+        btn.textContent = i + 1
+        if(i + 1 === state.page || i === 1 && state.page === 5) {
+            btn.classList.add("page-info")
+            btn.textContent = "..."
+        }
+    })
+}
+render()
